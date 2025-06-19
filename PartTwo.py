75d62596ceb95cfc6f2d1af649715cbcc5767cdc
eledelaf <eledelaf@ucm.es>
2025-06-19 15:40:17 +0100

    Prediction

--- PartTwo.py
+++ PartTwo.py
@@ -48,6 +48,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
+from sklearn.metrics import f1_score
 
 
 vectorizer = TfidfVectorizer(stop_words = "english", max_features = 3000)
@@ -71,9 +72,12 @@ trying to predict is the ‘party’ value.
 # Train RandomForest (with n_estimators=300)
 r_Forest = RandomForestClassifier(n_estimators=300)
 r_Forest.fit(X_train, y_train)
+r_F_predict = r_Forest.predict(X_test)
 
 # Train the SVM with linear kernel classifiers
 svm = SVC(kernel= 'linear')
 svm.fit(X_train, y_train)
+svm_predict = svm.predict(X_test)
 
+# Print the scikit-learn macro-average f1 score
 
