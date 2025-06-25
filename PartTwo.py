f6fc7989102b2f9c3488bacb8f2b5e90c2369ed5
eledelaf <eledelaf@ucm.es>
2025-06-25 09:42:54 +0100

    Some changes in partTwo

--- PartTwo.py
+++ PartTwo.py
@@ -6,7 +6,6 @@ Sub-set and rename the dataframe as follows:
 import pandas as pd
 
 df = pd.read_csv("/Users/elenadelafuente/Desktop/MASTER/2 trimestre/Natural Lenguage Processing/Assesment/p2-texts/hansard40000.csv")
-#print(df.head())
 
 """
 Rename the ‘Labour (Co-op)’ value in ‘party’ column to ‘Labour’
@@ -15,32 +14,36 @@ df = df.replace({"Labour (Co-op)": "Labour"})
 
 """
 remove any rows where the value of the ‘party’ column is not one of the
-four most common party names remove any rows where the value in the ‘speech_class’ column is not
-‘Speech’.
+four most common party names, and remove the ‘Speaker’ value.
 """
-# Esto hay que cabiarlo, borrar primero lo de Speaker y luego buscar los 4 parties más importantes
 df = df[df["party"]!= "Speaker"]
 parties_4 = list(df['party'].value_counts()[:4].index)
 df = df[df['party'].isin(parties_4)]
-print(df.head())
+
+"""
+remove any rows where the value in the ‘speech_class’ column is not
+‘Speech’.
+"""
+df = df[df['speech_class'] == 'Speech']
 
 """
 remove any rows where the text in the ‘speech’ column is less than 1000
 characters long.
 """
 final_df = df[df["speech"].str.len() >= 1000]
-# final_df = df.drop(df[df["speech"].str.len() < 1000].index)
 
 """
 Print the dimensions of the resulting dataframe using the shape method.
 """
-print(final_df.head())
-print(final_df.shape)
+
+print('The shape of the final dataframes is: ', final_df.shape)
 
 """
 Vectorise the speeches using TfidfVectorizer from scikit-learn. Use the default
 parameters, except for omitting English stopwords and setting max_features to
 3000.
+Split the data into a train and test set, using stratified sampling, with a
+random seed of 26.
 """
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
@@ -53,12 +56,7 @@ from sklearn.metrics import classification_report
 vectorizer = TfidfVectorizer(stop_words = "english", max_features = 3000)
 
 X = vectorizer.fit_transform(final_df["speech"])
-"""
-Split the data into a train and test set, using stratified sampling, with a
-random seed of 26.
-"""
-# Since we are trying to predict the political party that said certain speech
-y = final_df["party"]
+y = final_df["party"] # Since we are trying to predict the political party that said certain speech
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=26, stratify=y)
 
